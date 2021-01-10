da9d9fa0d382b9c21472bae561e6d67840d490aa
NorhanHussien242 <77235263+NorhanHussien242@users.noreply.github.com>
2021-01-10 19:55:32 +0200

    Update prohu1.asm

--- Projectcal/prohu1.asm
+++ Projectcal/prohu1.asm
@@ -17,7 +17,7 @@ atoi PROTO C strptr:DWORD
 include irvine32.inc
 
 .data
-A_l=100 ;Array length
+A_l=100                                                         ;Array length
 string_equ BYTE A_l+1 DUP(?), 0                                 ; string input equation
 s_temp1 BYTE A_l+1 DUP(?), 0                                    ; temporary use 
 s_temp2 BYTE A_l+1 DUP(?), 0
@@ -52,13 +52,13 @@ main ENDP
 
 step_mul PROC
 MOV mul_result, 1 
-xor esi, esi 
+xor esi, esi                                                      ; will always set esi to zero , more effecient
 xor edi, edi
 xor ecx, ecx
-lea edx, s_temp2 
+lea edx, s_temp2                                                  ; Loads EDX with the offset address of s_temp2
 L1:
 	mov bl, [s_temp1 + esi] 
-	CMP bl, 0 ; if bl is 0
+	CMP bl, 0                                                 ; if bl is 0
 	JE End_Cont 
 	cmp bl, s_mul 
 	JE MUL_Cont 
@@ -68,13 +68,13 @@ L1:
 	inc ecx
 	
 	Inc_lbl :
-inc esi
-inc edi
-JMP L1
+         inc esi
+         inc edi
+         JMP L1
 
 MUL_Cont : 
-lea edx, s_temp2
-call ParseInteger32
+lea edx, s_temp2                                                   ; Loads EDX with the offset address of s_temp2
+call ParseInteger32                                                ;convert string up to length to integer , take edx ,ecx as arguments,return in eax
 cmp r_mul, '*'
 JE MUL_RES
 MOV parse_result, EAX
@@ -84,11 +84,11 @@ IDIV parse_result
 MOV mul_result, EAX
 JMP Cont_MUL
 MUL_RES :
-IMUL EAX, mul_result
+IMUL EAX, mul_result                                              ;use IMUL because it takes to arguments 
 MOV mul_result, EAX
 Cont_MUL : 
 MOV r_mul, '*'
-MOV edi, -1
+MOV edi, -1                                                       ;to make it zero later
 mov ecx, 0
 push eax
 call resetstemp2
