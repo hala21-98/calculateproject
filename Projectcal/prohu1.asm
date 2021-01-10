fcb00cb79df4e21840782dc65aeb5b9625dd2225
NorhanHussien242 <77235263+NorhanHussien242@users.noreply.github.com>
2021-01-10 18:45:46 +0200

    third part of code

--- Projectcal/prohu1.asm
+++ Projectcal/prohu1.asm
@@ -99,4 +99,56 @@ call resetstemp2
 pop eax
 JMP Inc_lbl
 	
-	
+End_Cont :
+lea edx, s_temp2
+call ParseInteger32
+cmp r_mul, '*'
+JE MUL_RES3
+MOV parse_result, EAX
+MOV EAX, mul_result
+mov edx, 0
+IDIV parse_result
+MOV mul_result, EAX
+JMP Cont_F
+MUL_RES3 :
+IMUL EAX, mul_result
+MOV mul_result, EAX
+Cont_F :
+push eax
+call resetstemp2
+pop eax
+ret
+step_mul ENDP
+
+step_add PROC
+xor esi, esi
+xor edi, edi
+L1 :
+mov cl, [edx + esi]
+CMP cl, 0
+JE End_Cont
+cmp cl, s_add
+JE ADD_Cont
+cmp cl, s_sub
+JE SUB_Cont
+mov[s_temp1 + edi], cl
+Inc_lbl :
+inc esi
+inc edi
+JMP L1
+
+ADD_Cont :
+push esi
+push edi
+push edx
+push ecx
+call step_mul
+pop ecx
+pop edx
+pop edi
+pop esi
+cmp r_add, '+'
+JE ADD_RES
+MOV EAX, mul_result
+SUB add_result, EAX
+JMP Cont_ADD	
